1d9cbe61e5189529c46d90928f1f8be46df65a8b
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-12-24 12:04:14 +0200

    set requested camera size

--- app/src/main/java/com/enterprises/wayne/simplefacedetectorexample/FaceTrackingService.java
+++ app/src/main/java/com/enterprises/wayne/simplefacedetectorexample/FaceTrackingService.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
@@ -17,6 +17,7 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.common.images.Size;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.face.FaceDetector;
@@ -28,26 +29,27 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
+
+import static android.hardware.Camera.getCameraInfo;
+import static android.hardware.Camera.getNumberOfCameras;
 
 /**
  * a background service that tracks faces and feeds the info of each instance to a FaceTracker
  */
-public class FaceTrackingService extends Service implements TrackerListener
-{
+public class FaceTrackingService extends Service implements TrackerListener {
     /* fields */
     private CameraSource mCameraSource;
     private FaceDetector mDetector;
     private DeviceIdleReceiver receiver;
     private PowerManager.WakeLock wakeLock;
 
-    public FaceTrackingService()
-    {
+    public FaceTrackingService() {
     }
 
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
 
         Log.e("Game", "onCreate");
@@ -68,8 +70,7 @@ public class FaceTrackingService extends Service implements TrackerListener
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
 
         Log.e("Game", "onDestroy");
@@ -80,8 +81,7 @@ public class FaceTrackingService extends Service implements TrackerListener
     /**
      * sets up the detector and the camera source and starts monitoring for faces
      */
-    public void startTracking()
-    {
+    public void startTracking() {
         Log.e("Game", "starting tracking");
 
         // setup the face tracker
@@ -95,39 +95,80 @@ public class FaceTrackingService extends Service implements TrackerListener
                         .build());
         Log.e("Game", "detector operational " + mDetector.isOperational());
 
+
+        // get the maximum preview sizes of the font facing camera
+        Camera.Size optimalCameraSize = null;
+        int frontFacingCameraId = getFrontFacingCameraId();
+        if (frontFacingCameraId != -1) {
+            android.hardware.Camera camera = android.hardware.Camera.open(frontFacingCameraId);
+            Camera.Parameters params = camera.getParameters();
+            List<Camera.Size> sizes = params.getSupportedPictureSizes();
+            optimalCameraSize = getMaxCameraSize(sizes);
+        }
+        if (optimalCameraSize != null)
+            Log.e("Game", "optimal size " + optimalCameraSize.width + " " + optimalCameraSize.height);
+
         // setup camera source
-        mCameraSource = new CameraSource.Builder(this, mDetector)
+        CameraSource.Builder cameraBuilder = new CameraSource.Builder(this, mDetector)
                 .setFacing(CameraSource.CAMERA_FACING_FRONT)
-                .setRequestedFps(10.0f)
-                .build();
+                .setRequestedFps(10.0f);
+        if (optimalCameraSize != null)
+            cameraBuilder.
+                    setRequestedPreviewSize(optimalCameraSize.width, optimalCameraSize.height);
+        mCameraSource = cameraBuilder.build();
 
         // start the camera source
-        try
-        {
+        try {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                    ||ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            {
+                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 Log.e("Game", "camera permission not granted");
                 stopSelf();
             }
             mCameraSource.start();
             Log.e("Game", "camera source started");
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("Game", "error starting camera source " + e.getMessage());
             Crashlytics.getInstance().logException(e);
         }
     }
 
     /**
+     * @return the size with the biggest width*height
+     */
+    private Camera.Size getMaxCameraSize(List<Camera.Size> sizes) {
+        long maxSize = 0;
+        Camera.Size max = null;
+        for (Camera.Size size : sizes)
+            if (max == null
+                    || 1l * size.width * size.height > maxSize) {
+                max = size;
+                maxSize = 1l * size.width * size.height;
+            }
+        return max;
+    }
+
+    /**
+     * @return the id of the front facing camera, the id is the same as its position
+     */
+    public int getFrontFacingCameraId() {
+        int numberOfCameras = getNumberOfCameras();
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT)
+                return i;
+        }
+        return -1;
+    }
+
+
+    /**
      * releases the face detector and the camera
      */
-    public void stopTracking()
-    {
+    public void stopTracking() {
         Log.e("Game", "stop tracking");
 
-        if (mCameraSource != null)
-        {
+        if (mCameraSource != null) {
             mCameraSource.release();
             mDetector.release();
             mCameraSource = null;
@@ -137,18 +178,15 @@ public class FaceTrackingService extends Service implements TrackerListener
     /**
      * takes a picture and schedule another one after that delay
      */
-    private void takePicture(final int faceId)
-    {
+    private void takePicture(final int faceId) {
         // check if stopped tracking
         if (mCameraSource == null)
             return;
 
         // take a picture
-        mCameraSource.takePicture(null, new CameraSource.PictureCallback()
-        {
+        mCameraSource.takePicture(null, new CameraSource.PictureCallback() {
             @Override
-            public void onPictureTaken(byte[] bytes)
-            {
+            public void onPictureTaken(byte[] bytes) {
                 savePicture(bytes, faceId);
             }
         });
@@ -159,18 +197,15 @@ public class FaceTrackingService extends Service implements TrackerListener
      * saves to a file(you'll find it in internal storage/pictures
      * the file name will be something like FaceTracker 03-18-00 (hour-minutes-seconds)
      */
-    private void savePicture(byte[] bytes, int faceId)
-    {
+    private void savePicture(byte[] bytes, int faceId) {
         // create a file for the image
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh-mm-ss");
         String currentDate = simpleDateFormat.format(Calendar.getInstance().getTime());
         String fileName = "FaceTracker " + faceId + " " + currentDate + ".png";
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), fileName);
-        try
-        {
+        try {
             file.createNewFile();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
             Crashlytics.getInstance().logException(e);
         }
@@ -178,41 +213,33 @@ public class FaceTrackingService extends Service implements TrackerListener
 
         // write the image to the file
         OutputStream fOut = null;
-        try
-        {
+        try {
             fOut = new FileOutputStream(file.getAbsolutePath());
             fOut.write(bytes);
 
             // ask the media scanner to include this file so we can see it in the file explorer
             MediaScannerConnection.scanFile(this,
                     new String[]{file.toString()}, null,
-                    new MediaScannerConnection.OnScanCompletedListener()
-                    {
-                        public void onScanCompleted(String path, Uri uri)
-                        {
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
                             Log.i("ExternalStorage", "Scanned " + path + ":");
                             Log.i("ExternalStorage", "-> uri=" + uri);
                         }
                     });
 
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             Log.e("Game", "error saving picture " + e.getMessage());
             Crashlytics.getInstance().logException(e);
             e.printStackTrace();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             Log.e("Game", "error saving picture " + e.getMessage());
             Crashlytics.getInstance().logException(e);
             e.printStackTrace();
-        } finally
-        {
-            try
-            {
+        } finally {
+            try {
                 fOut.flush();
                 fOut.close();
-            } catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
                 Crashlytics.getInstance().logException(e);
             }
@@ -223,8 +250,7 @@ public class FaceTrackingService extends Service implements TrackerListener
     /**
      * registers a receiver that stops tracking if the device is idle
      */
-    public void addDeviceIdleMonitor()
-    {
+    public void addDeviceIdleMonitor() {
         // create the intent filter
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -237,14 +263,12 @@ public class FaceTrackingService extends Service implements TrackerListener
 
 
     @Override
-    public void onUpdate(int faceId)
-    {
+    public void onUpdate(int faceId) {
         takePicture(faceId);
     }
 
     @Override
-    public IBinder onBind(Intent intent)
-    {
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
@@ -253,11 +277,9 @@ public class FaceTrackingService extends Service implements TrackerListener
      * starts tracking if the screen became active
      * stops tracking if the screen goes idle
      */
-    class DeviceIdleReceiver extends WakefulBroadcastReceiver
-    {
+    class DeviceIdleReceiver extends WakefulBroadcastReceiver {
         @Override
-        public void onReceive(Context context, Intent intent)
-        {
+        public void onReceive(Context context, Intent intent) {
             Log.e("Game", "device idle monitor" + intent.getAction());
             if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
                 startTracking();
